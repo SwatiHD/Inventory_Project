e915b5e0906314c7220cede62c9025a9ffdd5bdb
SwatiHD <swatihd1208@gmail.com>
2026-06-07 20:13:11 +0530

    Inventory env

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,20 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Shanthika@12345",
-  database: "inventory_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("DB ERROR:", err.message);
+  } else {
+    console.log("Database Connected");
+    connection.release();
+  }
 });
 
 module.exports = db.promise();
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const cors = require("cors");
 
 const itemRoutes = require("./routes/itemRoutes");
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 const app = express();
 
 app.use(cors());
@@ -10,6 +15,8 @@ app.use(express.json());
 
 app.use("/api", itemRoutes);
 
-app.listen(5000, () => {
-  console.log("Server Running on Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
 });
